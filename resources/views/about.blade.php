91388ea9a1c5a21bb1e1fd7cc4be19f00d9de4c4
TurkiAlotaibi <66381975+TurkiAlotaibi@users.noreply.github.com>
2020-07-05 12:08:55 +0300

    ,

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Training Group Website</title>
+        <title>Training Group Website </title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
